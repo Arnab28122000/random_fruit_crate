1af694fbc2c676aedadf4fa6fb202cfcf07b42e3
arnab28122000 <arnab.bhattacharya28122000@gmail.com>
2022-08-27 11:06:31 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+cargo doc --open
+cargo test
+
+cargo login token  /// generate token from crates.io
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+//! # My Crate
+//! 
+//! `mycrate` is a collection of utilities to make performing certion
+//! calculations more convinient.
+
+///Adds one to the number given
+/// 
+/// # Examples
+/// 
+/// ```
+/// let arg = 5;
+/// let answer = arnab_first_crate::add_one(arg);
+/// 
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32{
+    x+1
+}
